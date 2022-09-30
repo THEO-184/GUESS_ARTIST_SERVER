2c60825561111c1920367e7651025ed5dfbf0eff
Theophilus Boakye <93124888+TheophilusBoakye@users.noreply.github.com>
2022-09-30 13:32:54 +0000

    run build

--- build/app.js
+++ build/app.js
@@ -29,7 +29,7 @@ const app = (0, express_1.default)();
 app.use((0, morgan_1.default)("tiny"));
 app.use((0, cors_1.default)({
     credentials: true,
-    origin: "http://tiresome-mass.surge.sh/",
+    origin: "http://tiresome-mass.surge.sh",
 }));
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
